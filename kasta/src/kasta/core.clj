9e58e6db50a136a57aa26c7436e677c4121dff02
SavitskiyEV <38917889+SavitskiyEV@users.noreply.github.com>
2019-05-15 01:28:53 +0300

    fixed styling according to request

--- kasta/src/kasta/core.clj
+++ kasta/src/kasta/core.clj
@@ -1,65 +1,55 @@
 (ns kasta.core
-  (:require [clj-http.client :as client])
-  (:require [clj-time.core :as t])
-  (:require [clj-time.local :as l])
+  (:require [clj-http.client :as client]
+            [clj-time.core :as t]
+            [clj-time.local :as l])
   (:gen-class))
 
 (def menu-url "https://modnakasta.ua/api/v2/market/menu/")
-
 (def page-url "https://modnakasta.ua/market/")
-
 (def category-products-url "https://modnakasta.ua/api/v2/product-list")
 
 (def max-expected-node-products-count 10)
+(def *nodes-count* 0)
+(def *matching-nodes-count* 0)
 
-(def nodes-count 0)
-(def small-categories-count 0)
+(defn get-node-products-count [node]
+  (-> (clojure.string/join [category-products-url ( :q node)])
+    (client/get {:as :json})
+    :body
+    :product-ids
+    (count)))
 
-(defn get-node-products-count [node] 
-  (def node-product-url ( clojure.string/join [category-products-url ( :q node)]))
-  (def node-products ( :product-ids ( :body (client/get node-product-url {:as :json}))))
-
-  ( count node-products)
-)
-
-(defn process-leaf-node [node breadcrumbs]
-         
-  (def products-count (get-node-products-count node) )
-
-  (if ( < products-count max-expected-node-products-count) 
-    (do 
-      (println (clojure.string/join (clojure.string/join " > " 
-      (conj (vec (map :name breadcrumbs )) ( :name node) ))) "|" 
-        (clojure.string/replace (clojure.string/join [page-url ( :url node)]) #" " ""))
-        (def small-categories-count (+ small-categories-count 1))
-    )
-  )
-)
+(defn process-leaf-node [node breadcrumbs]         
+  (when ( < (get-node-products-count node) max-expected-node-products-count) 
+     (println (clojure.string/join (clojure.string/join " > " 
+      (conj (vec (map :name breadcrumbs)) ( :name node)))) "|" 
+        (clojure.string/replace (clojure.string/join [page-url 
+                                                      ( :url node)]) 
+                                  #" " ""))
+        (alter-var-root #'*matching-nodes-count* (constantly (inc *matching-nodes-count*)))))
 
 (defn parse-menu [response-body]
-;(println nodes-count)
-  (def breadcrumbs ( :breadcrumbs response-body)) 
-  (def menu-nodes ( :nodes response-body))
-  (def grouped-nodes ((juxt filter remove) #(= ( :has-children %) true) menu-nodes))
+  (def grouped-nodes 
+    ((juxt filter remove) #(= ( :has-children %) true) ( :nodes response-body)))
+  ;I have no idea how to get rid off that def and keep code simple. I mean, let 
+  ; with lambda that does all code below is a solution but I am unsure. Answer is welcome!
   (doseq [child-node (first grouped-nodes) ] 
-     @(parse-menu ( :body (client/get menu-url {:as :json, 
-                                                :query-params{"v" "1", 
-                                                              "parent-uuid" 
-                                                                ( :uuid child-node)}}))))
-
-  (doseq [leaf (second grouped-nodes)] (process-leaf-node leaf breadcrumbs) )
-  (def nodes-count (+ nodes-count (count ( :nodes response-body))))
-)
+          (parse-menu 
+           ( :body (client/get menu-url {:as :json, 
+                                        :query-params{"v" "1", 
+                                                      "parent-uuid" 
+                                                        ( :uuid child-node)}}))))
+  (doseq [leaf (second grouped-nodes)] 
+    (process-leaf-node leaf 
+      ( :breadcrumbs response-body)))
+  (alter-var-root #'*nodes-count* (constantly (inc *nodes-count*))))
       
 
 (defn -main
   "Processing kasta market and printing nodes route depending on their content"
   [& args]  
-  (println (l/local-now))
-  (def root-menu ( :body (client/get menu-url {:as :json, :query-params {"v" "1"}})))
-  (parse-menu root-menu)
-  (print (* 100 (float ( / small-categories-count nodes-count))))
-  (print " % из ")
-  (print nodes-count)
-  (print " категорий")
-  )
+  (parse-menu ( :body (client/get menu-url {:as :json, :query-params {"v" "1"}})))
+  (println (clojure.string/join [(* 100 (float (/ *matching-nodes-count* *nodes-count*))) 
+                                " % из " 
+                                *nodes-count* 
+                                " категорий"])))
